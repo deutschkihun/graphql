0856b75332d2af2395343135a3005113f7c8872e
KIHUN KIM <kihunkim@KIHUNui-MacBookAir.local>
2023-05-06 13:35:42 +0900

    Add 2-modularized apollo server

---
+++ 2-modularized-apollo-server /typedefs-resolvers/_queries.js
@@ -0,0 +1,29 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    equipments: [Equipment]
+    equipmentAdvs: [EquipmentAdv]
+    softwares: [Software]
+    software: Software
+    supplies: [Supply]
+    givens: [Given]
+    people: [People]
+    peopleFiltered(
+      team: Int
+      sex: Sex
+      blood_type: BloodType
+      from: String
+    ): [People]
+    peoplePaginated(
+      team: Int
+      sex: Sex
+      blood_type: BloodType
+      from: String
+      page: Int!
+      per_page: Int!
+    ): [People]
+  }
+`;
+
+module.exports = typeDefs;
